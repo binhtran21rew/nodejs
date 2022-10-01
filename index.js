b2ed0f37aa9283b4fde087cdec89f5e6896b8778
binhtran21rew <102853177+binhtran21rew@users.noreply.github.com>
2022-10-02 00:32:21 +0700

    add morgan

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const morgan = require('morgan')
 
 app.get('/home', (req, res) => {
     return res.send('hello guys')})
+
+app.use(morgan('combined'))
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
